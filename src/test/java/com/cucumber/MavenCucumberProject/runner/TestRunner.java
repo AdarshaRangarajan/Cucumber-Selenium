632eb9b75a179999e3975a42d0f270ffcdab5d3e
AdarshaRangarajan <adarsh.iyengar87@gmail.com>
2017-01-01 21:16:04 -0500

    added a runner class package to make sure my git commit from source tree works

---
+++ src/test/java/com/cucumber/MavenCucumberProject/runner/TestRunner.java
@@ -0,0 +1,10 @@
+package com.cucumber.MavenCucumberProject.runner;
+
+public class TestRunner {
+	
+  public void printStatement() {
+	  
+	  System.out.println("this is just to ensure the runner class file is pished to git");
+  }
+
+}
